1b673ead563083fcabb8f9eea94febb7f29012f7
coder164 <levmas92@gmail.com>
2023-08-23 16:45:08 +0300

    .

--- github_folder/c_projects/5exercises/interview_question2.c
+++ github_folder/c_projects/5exercises/interview_question2.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <math.h>   /* for EvaluateNumber() */
+#include <math.h>   /* for pow() */
 #define ARRAY_SIZE (7)
 #define INT_SIZE_IN_BITS (32)
 #define BIGGEST_POWER (INT_SIZE_IN_BITS - 1)
@@ -9,7 +9,8 @@ question:
 suppose you have an array of ints where except for one integer all the
 others occurs triple times in the array.
 how will you find the single value with complexity of O(n)?
-answer:
+
+first method:
 declare array of 32 inegers and initialize all values to zeroes.
 then loop over the given array 32 times and in each iteration if
 the most left bit is 1 then raise the current place in the
@@ -24,31 +25,40 @@ the '0' representing only shift of the number by one bit.
 then if you do on each value in that array %3 then only the
 single integer will be represented. so next thing is to extract that
 number from that array.
+
+the second method is very similar:
+declare array of 32 inegers and initialize all values to zeroes.
+then uprise by one the index of what bit is on in each number.
+when it finishes then your bits array will have
+something like 
+[3, 3, 3, 0, 0, 3, 1, 0, 3, 0, 1, 0, 3, 3, 1, 0, 3 ..., 0]
+if you do %3 on each valu in the bits array then all of the triplets
+will be zero and you will left with only the bits that representing
+the single number. then you can extract that and you will get your
+number. 
 */
 
-unsigned int FirstMethod(unsigned int _givenArray[],
-    unsigned int _arraySize);
-
-unsigned int SecondMethod(const unsigned int _givenArray[],
-    unsigned int _arraySize);
-
+unsigned int FirstMethod( int _givenArray[], int _arraySize);
+unsigned int SecondMethod(const int _givenArray[], int _arraySize);
 void PrintArrayInBits(unsigned int _arr[], unsigned int _size);
 unsigned int EvaluateNumber(unsigned int _arr[], unsigned int _size);
 
 int main()
 {
-    unsigned int givenArray[ARRAY_SIZE] = {1, 1, 1, 5, 3, 3, 3};
-    unsigned int desiredNumber = 0;
+    int givenArray[ARRAY_SIZE] = {1, 1, 1, 2147483646, 3, 3, 3};
+    int desiredNumber = 0;
+
     desiredNumber = SecondMethod(givenArray, ARRAY_SIZE);
     printf("Desired number = %d from SecondMethod()\n", desiredNumber);
+
     desiredNumber = 0;
     desiredNumber = FirstMethod(givenArray, ARRAY_SIZE);
     printf("Desired number = %d from FirstMethod()\n", desiredNumber);
+
     return 0;
 }
 
-unsigned int SecondMethod(const unsigned int _givenArray[],
-    unsigned int _arraySize)
+unsigned int SecondMethod(const int _givenArray[], int _arraySize)
 {
     unsigned int bitsArray[INT_SIZE_IN_BITS] = {0};
     unsigned int i, j;
@@ -66,8 +76,7 @@ unsigned int SecondMethod(const unsigned int _givenArray[],
     return EvaluateNumber(bitsArray, INT_SIZE_IN_BITS);
 }
 
-unsigned int FirstMethod(unsigned int _givenArray[],
-    unsigned int _arraySize)
+unsigned int FirstMethod(int _givenArray[], int _arraySize)
 {
     unsigned int bitsArray[INT_SIZE_IN_BITS] = {0};
     unsigned int i, j;
