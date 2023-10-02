b6202190aedb64cd12d9449aac21b51608860143
coder164 <levmas92@gmail.com>
2023-10-02 23:35:35 +0300

    .

--- github_folder/c_projects/5exercises/threads/my_shell_with_helper_prints.c
+++ github_folder/c_projects/5exercises/threads/my_shell_with_helper_prints.c
@@ -30,16 +30,23 @@ static void MyShell(void) {
 		commandsList[i] = strtok(command, delimiters);
 		while (NULL != commandsList[i])
 		{
+			printf("\tInner while\n");
+			printf("\tcommandsList[%d] = %s\n", i, commandsList[i]);
 			++i;
 			commandsList[i] = strtok(NULL, delimiters);
 		}
+		printf("outer while\n");
 		pid = fork();
-		if (pid > 0)		/* parent code */
+		if (pid > 0)
 		{
+			printf("\tparent started.\n");
 			waitpid(-1, &status, 0);
+			printf("\tparent code ended. status = %d\n", status);
 		}
-		else if (pid == 0)	/* child code */
+		else if (pid == 0)
 		{
+			printf("\t\tchild code\n");
+			printf("commandsList[0] = %s \t commandsList[1] = %s\n", commandsList[0], commandsList[1]);
 			execvp(command, commandsList);
 			printf("command not found\n");
 			break;
